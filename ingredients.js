e375bc249bf871977f8177f842b04ec163621dc3
sdsog <34170634+sdsog@users.noreply.github.com>
2019-05-07 23:12:44 +0000

    ingredients exercise - didm't save

--- ingredients.js
+++ ingredients.js
@@ -6,12 +6,10 @@ while (counter < ingredients.length) {
   counter++;
 }
 
-// Write a for loop that prints out the contents of ingredients:
 for (i = 0; i < ingredients.length; i++){
   console.log(ingredients[i]);
 }
 
-// Write any loop (while or for) that prints out the contents of ingredients backwards:
 for (i = (ingredients.length) - 1; i > -1; i--) {
   console.log(ingredients[i]);
 }
