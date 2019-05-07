f6e91d9752e017fb8aa31ac6057973c1ecd3d4e7
sdsog <34170634+sdsog@users.noreply.github.com>
2019-05-07 02:31:05 +0000

    add age calculator

---
+++ agecalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  console.log(name + " is " + (currentYear - yearOfBirth) + " years old.");
+}
+
+ageCalculator("Suzie", 1984, 2016);
+ageCalculator("Jack", 2004, 2016);
+ageCalculator("Ali", 2016, 2016);
