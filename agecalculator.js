7f35a64e4db82cea31c61521afcecd64e8696c94
sdsog <34170634+sdsog@users.noreply.github.com>
2019-05-14 19:55:33 +0000

    remove old agecalculator file

--- agecalculator.js
+++
@@ -1,7 +0,0 @@
-function ageCalculator(name, yearOfBirth, currentYear) {
-  console.log(name + " is " + (currentYear - yearOfBirth) + " years old.");
-}
-
-ageCalculator("Suzie", 1984, 2016);
-ageCalculator("Jack", 2004, 2016);
-ageCalculator("Ali", 2016, 2016);
